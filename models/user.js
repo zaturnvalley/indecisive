e5ca31dfdfd4a05645b2252f013eb1d4613179db
zaturnvalley <zacharycmason@gmail.com>
2016-10-03 18:22:58 -0700

    built user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+
+var UserSchema = mongoose.Schema({
+  username: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.set('toJSON', {
+  transform: function(doc, ret, options) {
+    var returnJson = {
+      id: ret._id,
+      email: ret.email,
+      username: ret.username
+    };
+    return returnJson;
+  }
+});
+
+UserSchema.methods.authenticated = function(password) {
+  var user = this;
+  var isAuthenticated = bcrypt.compareSync(password, username.password);
+  return isAuthenticated ? user : false;
+};
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')){
+    next();
+  } else {
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
