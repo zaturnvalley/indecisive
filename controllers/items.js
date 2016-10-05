59e117f6f78818dbfe50d8e99c6d4ea293b8bebf
zaturnvalley <zacharycmason@gmail.com>
2016-10-04 18:04:32 -0700

    get and post routes for item

--- controllers/items.js
+++ controllers/items.js
@@ -1,3 +1,28 @@
 var express = require('express');
 var Item = require('./models/item');
 var router = express.Router();
+
+router.route('/')
+  .get(function(req, res) {
+    Item.find(function(err, items) {
+      if (err) return res.status(500).send(err);
+
+      return res.send(items)
+    });
+  })
+  .post(function(req, res) {
+    Item.create(req.body, function(err, item) {
+      if (err) return res.status(500).send(err);
+
+      return res.send(item);
+    });
+  });
+
+  router.route('/:id')
+  .get(function(req, res){
+    Item.findById(req.params.id, function(err, item){
+      if (err) return res.status(500).send(err);
+
+      return res.send(item);
+    });
+  })
